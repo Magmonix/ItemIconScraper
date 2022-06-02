379e8419d9c1e1a97fe7ef5d43d9766f16fc188c
Magmonix <wowinsomnia@yahoo.com>
2022-06-02 10:19:38 -0500

    Add project files.

---
+++ ItemIconScraper/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ItemIconScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run_Button = new System.Windows.Forms.Button();
+            this.counter_Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Run_Button
+            // 
+            this.Run_Button.Location = new System.Drawing.Point(12, 12);
+            this.Run_Button.Name = "Run_Button";
+            this.Run_Button.Size = new System.Drawing.Size(75, 23);
+            this.Run_Button.TabIndex = 0;
+            this.Run_Button.Text = "Run";
+            this.Run_Button.UseVisualStyleBackColor = true;
+            this.Run_Button.Click += new System.EventHandler(this.Run_Button_Click);
+            // 
+            // counter_Label
+            // 
+            this.counter_Label.AutoSize = true;
+            this.counter_Label.Location = new System.Drawing.Point(147, 17);
+            this.counter_Label.Name = "counter_Label";
+            this.counter_Label.Size = new System.Drawing.Size(0, 13);
+            this.counter_Label.TabIndex = 1;
+            this.counter_Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 112);
+            this.Controls.Add(this.counter_Label);
+            this.Controls.Add(this.Run_Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run_Button;
+        private System.Windows.Forms.Label counter_Label;
+    }
+}
+
---
+++ ItemIconScraper/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net.Http;
+
+namespace ItemIconScraper
+{
+    public partial class Form1 : Form
+    {
+        public string source;
+        static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Run_Button_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            string iconString;
+            int itemId;
+            int itemIteration = 1;
+            int itemMaxIteration = 5855;
+
+            while (itemIteration <= itemMaxIteration)
+            {
+                counter_Label.Text = itemIteration.ToString();
+                HttpResponseMessage response = await client.GetAsync("https://wotlk.evowow.com/?icon=" + itemIteration.ToString());
+                string source = await response.Content.ReadAsStringAsync();
+
+                Match iconMatch = Regex.Match(source, "^.+(g_icons).+$", RegexOptions.Multiline);
+                Match itemMatch = Regex.Match(source, "^.+(g_items).+$", RegexOptions.Multiline);
+                if (iconMatch.Success && itemMatch.Success)
+                {
+                    List<string> iconIdBuilder = new List<string>();
+                    int i = iconMatch.Value.IndexOf(':');
+                    while (iconMatch.Value[i + 2] != '"')
+                    {
+                        iconIdBuilder.Add(iconMatch.Value[i + 2].ToString());
+                        i++;
+                    }
+                    iconString = String.Join("", iconIdBuilder);
+
+                    string original = itemMatch.Value;
+                    while (original.Contains('['))
+                    {
+                        StringBuilder sb = new StringBuilder(original);
+                        List<string> itemIdBuilder = new List<string>();
+                        int index = original.IndexOf('[');
+                        while (char.IsDigit(original[index + 1]))
+                        {
+                            itemIdBuilder.Add(original[index + 1].ToString());
+                            index++;
+                        }
+                        try
+                        {
+                            itemId = int.Parse(String.Join("", itemIdBuilder));
+                            dict.Add(itemId, iconString);
+                        }
+                        catch { }
+                        sb.Remove(original.IndexOf('['), 1);
+                        original = sb.ToString();
+                    }
+                }
+                foreach (var item in dict)
+                {
+                    File.AppendAllText("C:\\Users\\Magmonix\\Documents\\Test.txt", "{" + item.Key + " , \"" + item.Value + "\"},\n");
+                }
+                dict.Clear();
+                itemIteration++;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
